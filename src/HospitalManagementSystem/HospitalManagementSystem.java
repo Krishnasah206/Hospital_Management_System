ee517c038becd8cc06e9179f0ad56cb8cbfd6af5
Krishna Kumar Sah <krishnasah2060@gmail.com>
2025-06-07 11:59:02 +0530

    Update HospitalManagementSystem.java

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class HospitalManagementSystem {
     private static final String url = "jdbc:mysql://localhost:3306/hospital";
     private static final String username = "root";
-    private static final String password = "Avengers0602!";
+    private static final String password = your_password;  // Change this according to your sql password
 
     public static void main(String[] args) {
         try{
